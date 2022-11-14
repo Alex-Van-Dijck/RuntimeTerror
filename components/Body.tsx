8a3f4b12b90311f92f9344d720f53cd67bcddcb8
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2022-11-14 14:30:14 +0100

    Initial header design

---
+++ components/Body.tsx
@@ -0,0 +1,22 @@
+import {StyleSheet,View,Text} from 'react-native';
+
+const Body = ()=>{
+
+return(
+    <View>
+        <Text>
+
+        </Text>
+    </View>
+)
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:"column",
+    }
+});
+
+export default Body;
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,11 +9,11 @@ const Header = (props:IHeaderProps) =>{
 
     return(
             <View style={styles.container}>
-                <View style={styles.avatar}>
-                    <Image source={require(props.imageSource)}/>
+                <View>
+                    <Image style={styles.image} source={{uri: (props.imageSource)}}/>
                 </View>
                 <View>
-                    <Text>{props.name}</Text>
+                    <Text style={styles.name}>{props.name}</Text>
                 </View>
             </View>
     )
@@ -23,17 +23,19 @@ const Header = (props:IHeaderProps) =>{
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        flexDirection:"column",
-    },
-    avatar:{
-        width:80,
-        height:80,
-        backgroundColor:'red',
-        borderRadius:200,
-        margin:10,
+        flexDirection:"row",
     },
     name:{
-
+        marginTop:30,
+        marginLeft:5,
+        fontSize:20,
+        
+    },
+    image:{
+        width:60,
+        height:60,
+        borderRadius:200,
+        margin:10,
     }
 });
 

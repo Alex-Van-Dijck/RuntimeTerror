6955250e1fd0c4e4dcf6f7582284205687674931
Raven Van Hove <raven.vanhove@student.ap.be>
2022-11-14 17:20:28 +0100

    Load data from api

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Post from './components/Post';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import Post from "./components/Post";
+import { getData, IPost } from "./services/apiService";
 
-
-export default function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [posts, setPosts] = useState<IPost[]>([]);
+  useEffect(() => {
+    getData("posts")
+      .then((data) => setPosts(data))
+      .catch((error) => console.error(error))
+      .finally(() => setIsLoading(false));
+  }, []);
+  const numberInArray: number = 7;
   return (
     <View style={styles.container}>
-      <Post userImageSource='https://randomuser.me/api/portraits/med/men/3.jpg' userName='John Doe' bodyImageSource='https://img.dummyapi.io/photo-1564849444446-f876dcef378e.jpg' tags={[ 'plant', 'mammal', 'pet' ]} caption='A feral cat short-fur gray and orange cat on green...'/>
+      {isLoading ? (
+        <Text>Is loading...</Text>
+      ) : (
+        <ScrollView>
+          {posts.map((post: IPost, index) => (
+            <Post
+              key={index}
+              userImageSource={post.owner.picture}
+              userName={`${post.owner.firstName} ${post.owner.lastName}`}
+              bodyImageSource={post.image}
+              tags={post.tags}
+              caption={post.text}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
--- components/Post.tsx
+++ components/Post.tsx
@@ -2,26 +2,30 @@ import { Text, View, StyleSheet } from "react-native";
 import Header from "./Header";
 import Body from "./Body";
 
-interface IPostProps{
-    userImageSource:string,
-    userName:string,
-    bodyImageSource:string,
-    tags:string[],
-    caption:string,
+interface IPostProps {
+  userImageSource: string;
+  userName: string;
+  bodyImageSource: string;
+  tags: string[];
+  caption: string;
 }
 
-const Post = (props:IPostProps) =>{
-
-return(
-<>
-    <View style={styles.card} >
-        <Header imageSource={props.userImageSource} name={props.userName}/>
-        <Body imageSource={props.bodyImageSource} tags={props.tags} caption={props.caption}/>
-    </View>
-</>
-)
-
-}
+const Post = ({
+  userImageSource,
+  userName,
+  bodyImageSource,
+  tags,
+  caption,
+}: IPostProps) => {
+  return (
+    <>
+      <View style={styles.card}>
+        <Header imageSource={userImageSource} name={userName} />
+        <Body imageSource={bodyImageSource} tags={tags} caption={caption} />
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   card: {

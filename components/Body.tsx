30e739af8283d8d2eac76488c702e911a3b8882b
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2022-11-14 14:49:56 +0100

    Initial body

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,11 +1,21 @@
-import {StyleSheet,View,Text} from 'react-native';
+import {StyleSheet,View,Text,Image} from 'react-native';
 
-const Body = ()=>{
+interface IBodyProps{
+    imageSource:string,
+    tags:string[],
+    caption:string
+}
+
+const Body = (props:IBodyProps)=>{
 
 return(
-    <View>
+    <View style={styles.container}>
         <Text>
-
+            {props.caption}
+        </Text>
+        <Image style={styles.image} source={{uri:(props.imageSource)}}/>
+        <Text style={styles.tags}>
+            {props.tags.map((tag:string,key)=> <Text>#{tag} </Text>)}
         </Text>
     </View>
 )
@@ -16,6 +26,18 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         flexDirection:"column",
+        justifyContent:'space-around'
+    },
+    image:{
+        height:200,
+        width:200,
+        alignSelf:'center'
+    },
+    tags:{
+        textAlign:'left'
+    },
+    caption:{
+        textAlign:'left'
     }
 });
 
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,6 @@
 import {Text,View,StyleSheet} from 'react-native';
 import Header from './Header';
+import Body from './Body';
 
 
 const Post = () =>{
@@ -8,6 +9,7 @@ return(
 <>
     <View style={styles.card} >
         <Header imageSource='https://randomuser.me/api/portraits/med/men/3.jpg' name='John Doe'/>
+        <Body imageSource='https://img.dummyapi.io/photo-1564849444446-f876dcef378e.jpg' tags={[ 'plant', 'mammal', 'pet' ]} caption='A feral cat short-fur gray and orange cat on green...'/>
     </View>
         
 </>
@@ -18,13 +20,13 @@ return(
 const styles = StyleSheet.create({
     card:{
         display:'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         borderColor: 'blue',
         borderRadius: 50,
         borderWidth: 2,
         width:300,
         height:600,
-        justifyContent:'flex-start',
+        justifyContent:'center',
         backgroundColor:'lightgrey',
     }
   });

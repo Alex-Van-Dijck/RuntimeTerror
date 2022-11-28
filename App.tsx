a9cfb23a5410449609b98595e86430f1ec4794f9
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2022-11-28 14:45:01 +0100

    Added icons

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import HomeScreen from "./components/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import NewPostScreen from "./components/NewPostScreen";
+import { FontAwesome } from "@expo/vector-icons";
+import { Entypo } from '@expo/vector-icons';
 
 const App = () => {
 
@@ -13,11 +15,14 @@ const App = () => {
   return (
     <NavigationContainer >
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="New Post" component={NewPostScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} options={{
+            tabBarIcon: ({color, size}) => <FontAwesome name="home" size={size} color={color} />
+        }}  />
+          <Tab.Screen name="New Post" component={NewPostScreen} options={{
+            tabBarIcon: ({color, size}) => <Entypo name="camera" size={size} color={color} />
+        }}   />
         </Tab.Navigator>
     </NavigationContainer>
-    
   );
 };
 

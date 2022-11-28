e7d1ae4b08f1e190379d1e8cd9ac9fa87c3cf434
Raven Van Hove <raven.vanhove@student.ap.be>
2022-11-28 14:41:10 +0100

    pre-merge

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import Post from "./components/Post";
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from "react-native";
 import { getData, IPost } from "./services/apiService";
 import HomeScreen from "./components/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,14 +9,16 @@ const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [posts, setPosts] = useState<IPost[]>([]);
   const [onlyCreated, setOnlyCreated] = useState<boolean>(false);
+
   useEffect(() => {
     getData("posts", onlyCreated)
       .then((data) => setPosts(data))
       .catch((error) => console.error(error))
       .finally(() => setIsLoading(false));
   }, []);
-  const numberInArray: number = 7;
+
   const Tab = createBottomTabNavigator();
+
 
   return (
     <NavigationContainer>
